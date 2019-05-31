3cbb8a49ac9678d32167ada53ab5f8b54ecb01c8
mmshabab90 <mmshabab90@gmail.com>
2019-06-01 00:43:04 +0200

    changed environment.js to try and run|deploy app fro mgithub

--- config/environment.js
+++ config/environment.js
@@ -45,7 +45,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
-    ENV.rootURL = '/REPOSITORY_NAME/';
+    ENV.rootURL = '/football-simulator/';
   }
 
   return ENV;
